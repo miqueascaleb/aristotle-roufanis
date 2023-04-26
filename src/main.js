ceb5f1735c3a6c37983ee13e8710255f74b93006
Miqueas Prieto <sucksf8@gmail.com>
2023-04-25 21:06:46 -0300

    added whole repository

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+
+const artwork = document.querySelectorAll(".artwork");
+const titleBox = document.querySelector(".artwork-title");
