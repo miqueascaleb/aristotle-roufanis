966d847fbdff4e24ab1d1d1e201d065fb6413b8f
Miqueas Prieto <sucksf8@gmail.com>
2023-06-14 12:17:06 -0300

    finished version

--- src/main.js
+++ src/main.js
@@ -1,5 +1,2 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
-
-const artwork = document.querySelectorAll(".artwork");
-const titleBox = document.querySelector(".artwork-title");
